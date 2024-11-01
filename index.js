270ac73edbe4d2097ec5a8d039847e5272dc42fc
Miguel Tibincoski <tibincoskimiguel@gmail.com>
2024-11-01 00:31:10 -0300

    feat: Update Discord bot startup process and logging

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-require("dotenv").config()
-const manager = new (require('discord.js')).ShardingManager('./bot.js', { token: process.env.DISCORD_BOT_TOKEN });
-manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
+require("dotenv").config();
+const console = require("./services/console");
+
+console.log("------------------------------------")
+
+const manager = new (require("discord.js").ShardingManager)("./bot.js", {
+  token: process.env.DISCORD_BOT_TOKEN,
+});
+manager.on("shardCreate", (shard) =>
+  console.info(`Launched shard ${shard.id}`)
+);
 manager.spawn();
