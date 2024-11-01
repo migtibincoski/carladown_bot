d09bbeccd839eec5d0b90cf1938257ed342c4e2a
Miguel Tibincoski <tibincoskimiguel@gmail.com>
2024-11-01 00:28:52 -0300

    feat: Add console logging utility module

---
+++ services/console.js
@@ -0,0 +1,20 @@
+module.exports = {
+  log: function (message) {
+    console.log(`\x1b[1;37m${message}\x1b[0m`);
+  },
+  error: function (message) {
+    console.log(`\x1b[0;31m${message}\x1b[0m`);
+  },
+  info: function (message) {
+    console.log(`\x1b[1;34m${message}\x1b[0m`);
+  },
+  warn: function (message) {
+    console.log(`\x1b[1;33m${message}\x1b[0m`);
+  },
+  debug: function (message) {
+    console.log(`\x1b[0;33m${message}\x1b[0m`);
+  },
+  success: function (message) {
+    console.log(`\x1b[0;32m${message}\x1b[0m`);
+  },
+};
