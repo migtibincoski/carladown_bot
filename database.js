9a7af4d5ed185b171f7495e5d1ed28a17c5a083b
Miguel Tibincoski <tibincoskimiguel@gmail.com>
2024-11-01 00:30:30 -0300

    feat: Update MongoDB connection URI and add ShrtFly integration

--- database.js
+++ database.js
@@ -1,10 +1,9 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const { get } = require("axios");
 require("dotenv").config();
-const uri =
-  "mongodb+srv://dev:" +
-  process.env.MONGODB_PASSWORD +
-  "@ytdownbot.0ar7dgy.mongodb.net/?retryWrites=true&w=majority";
+
+const uri = `mongodb+srv://admin:${process.env.MONGODB_PASSWORD}@carladown.d3xwq.mongodb.net/?retryWrites=true&w=majority&appName=CarlaDown`;
+
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -14,27 +13,28 @@ const client = new MongoClient(uri, {
 });
 module.exports = {
   createURL: async (videoID, isMP3) => {
-    try {
-      await client.connect();
-      const myDB = client.db(
-        `ytdown_${
-          process.env.IS_PRODUCTION.toString() == "true" ? "prod" : "dev"
-        }`
-      );
-      const myColl = myDB.collection(`${isMP3 ? "mp3" : "mp4"}`);
-      const alreadyHaveThisVideo = await myColl.findOne({
-        videoID,
-      });
-      if (alreadyHaveThisVideo !== null) {
-        return {
-          error: null,
-          shortLink: alreadyHaveThisVideo.shortLink,
-        };
-      }
+    await client.connect();
+    const myDB = client.db(
+      `carladown_${
+        process.env.IS_PRODUCTION.toString() == "true" ? "prod" : "dev"
+      }`
+    );
 
-      const downloadID = `${Date.now()}_${Math.random()}`;
+    const myColl = myDB.collection(`${isMP3 ? "mp3" : "mp4"}`);
+    const alreadyHaveThisVideo = await myColl.findOne({
+      videoID,
+    });
 
-      const { data } = await get(
+    if (alreadyHaveThisVideo !== null) {
+      return {
+        error: null,
+        shortLink: alreadyHaveThisVideo.shortLink,
+      };
+    }
+
+    const downloadID = `${Date.now()}_${Math.random()}`;
+
+    /*const { data } = await get(
         "https://shrtfly.com/api?api=" +
           process.env.SHRTFLY_API_KEY +
           "&url=http://" +
@@ -44,28 +44,36 @@ module.exports = {
           "?id=" +
           downloadID +
           "&type=1&format=json"
-      );
+      );*/
 
-      let shortLink = data.result.shorten_url;
-      const result = await myColl.insertOne({ downloadID, videoID, shortLink });
-      await client.close();
-      return {
-        error: null,
-        shortLink,
-      };
-    } catch (err) {
-      console.error(err);
-      const error = err?.message || err;
-      return {
-        error,
-        shortLink: null,
-      };
-    }
+    const request = await fetch(
+      `https://shrtfly.com/api?api=${
+        process.env.SHRTFLY_API_KEY
+      }&url=${encodeURIComponent(
+        `http://${process.env.SERVER_DOMAIN}/api/download_${
+          isMP3 ? "mp3" : "mp4"
+        }?id=${downloadID}`
+      )}&type=1&format=json`
+    );
+
+    const data = await request.json();
+
+    const shortLink = data.result.shorten_url;
+    const result = await myColl.insertOne({
+      downloadID,
+      videoID,
+      shortLink,
+    });
+    await client.close();
+    return {
+      error: null,
+      shortLink,
+    };
   },
   getURL: async (downloadID, isMP3) => {
     await client.connect();
     const myDB = client.db(
-      `ytdown_${
+      `carladown_${
         process.env.IS_PRODUCTION.toString() == "true" ? "prod" : "dev"
       }`
     );
