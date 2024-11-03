7d22a8c862287776bb7e323553e73caef9e40bd5
Miguel Tibincoski <tibincoskimiguel@gmail.com>
2024-11-02 22:11:04 -0300

    refactor: Update console.js to improve logging functions

--- services/console.js
+++ services/console.js
@@ -1,21 +1,33 @@
 module.exports = {
   log: function (message) {
-    console.log(`\x1b[1;37m${message}\x1b[0m`);
+    console.log(`\x1b[1;37m`);
+    console.log(message);
+    console.log(`\x1b[0m`);
   },
   error: function (message) {
-    console.log(`\x1b[0;31m${message}\x1b[0m`);
+    console.error(`\x1b[0;31m`);
+    console.error(message);
+    console.error(`\x1b[0m`);
   },
   info: function (message) {
-    console.log(`\x1b[1;34m${message}\x1b[0m`);
+    console.info(`\x1b[1;34m`);
+    console.info(message);
+    console.info(`\x1b[0m`);
   },
   warn: function (message) {
-    console.log(`\x1b[1;33m${message}\x1b[0m`);
+    console.warn(`\x1b[1;33m`);
+    console.warn(message);
+    console.warn(`\x1b[0m`);
   },
   debug: function (message) {
-    console.log(`\x1b[0;33m${message}\x1b[0m`);
+    console.debug(`\x1b[0;33m`);
+    console.debug(message);
+    console.debug(`\x1b[0m`);
   },
   success: function (message) {
-    console.log(`\x1b[0;32m${message}\x1b[0m`);
+    console.log(`\x1b[0;32m`);
+    console.log(message);
+    console.log(`\x1b[0m`);
   },
   originalLog: console.log,
 };
